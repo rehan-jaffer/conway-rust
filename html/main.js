d032606b156a6a6c24f844187cce3f26213d63db
Rehan Jaffer <ray@thelondonvandal.com>
2018-03-14 11:44:25 +0000

    still working on HTML/JS wrapper for WASM code

--- html/main.js
+++ html/main.js
@@ -1,9 +1,34 @@
+const GRID_SIZE = 64 * 64;
+
+class Conway {
+  constructor(size) {
+    this.grid = new Uint8Array(GRID_SIZE);
+  }
+}
+
+class Wasm {
+  constructor() {
+    var memory = new WebAssembly.Memory({initial:256, maximum:256});
+    var importObject = {
+      imports: { imported_func: arg => console.log(arg) },
+      env: {
+        memory: memory
+      }	
+    };
+    WebAssembly.instantiateStreaming(
+      fetch(
+        "http://localhost/conway/conway.wasm",
+        {
+          headers: new Headers({ "Content-Type": "application/wasm" })
+        },
+      ).then(obj => {
+        return obj;
+      }), importObject);
+
+  }
+}
+
 $(document).ready(function() {
-
-  WebAssembly.instantiateStreaming(fetch('myModule.wasm'), importObject)
-  .then(obj => {
-  // Call an exported function:
-    obj.instance.exports.gr();
-})
-
+  let wasm = new Wasm();
+  let conway = new Conway();
 });
